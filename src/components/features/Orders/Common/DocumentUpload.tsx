8f3537952680a12bf824a931caf8c057ecc8576f
Raghvendra Roy <rroy@wcss.com>
2026-01-05 09:51:34 +0530

    Tracking Field fix

--- src/components/features/Orders/Common/DocumentUpload.tsx
+++ src/components/features/Orders/Common/DocumentUpload.tsx
@@ -56,127 +56,89 @@ export default function DocumentUpload({
   };
 
   return (
-    <Grid size={{ xs: 12, md: 12 }}>
-      <FormControl
-        fullWidth
-        variant="outlined"
+    <FormControl
+      fullWidth
+      sx={{
+        border: "1px solid #C7C9CD",
+        borderRadius: 1,
+        px: 2,
+        py: 1.5,
+      }}
+    >
+      <InputLabel shrink>Upload Document *</InputLabel>
+
+      <Box
         sx={{
-          "& .MuiOutlinedInput-root": {
-            "& fieldset": { border: "1px solid #C7C9CD" },
-            "&:hover fieldset": { border: "1px solid #C7C9CD" },
-            "&.Mui-focused fieldset": { border: "1px solid #C7C9CD" },
-          },
+          mt: 3,
+          display: "flex",
+          flexDirection: "column",
+          gap: 1.5,
         }}
       >
-        <InputLabel shrink>Upload Document *</InputLabel>
+        {/* File Upload */}
+        <ValidatedFileUpload
+          label="Choose file"
+          fileNameProp={fileName}
+          onChange={handleFileChange}
+        />
 
-        <OutlinedInput
-          notched
-          label="Upload Document"
-          inputComponent={() => (
-            <Box
-              sx={{
-                px: 2,
-                py: 1.5,
-                display: "flex",
-                flexDirection: "column",
-                gap: 1.5,
-                width: "100%",
-              }}
-            >
-              {/* File Upload */}
-              <Box sx={{ mb: 1 }}>
-                <ValidatedFileUpload
-                  label="Choose file"
-                  fileNameProp={fileName}
-                  onChange={handleFileChange}
-                />
-              </Box>
+        {/* Nested Options */}
+        <RadioGroup
+          value={nestedSelection}
+          onChange={(e) => setNestedSelection(e.target.value as any)}
+        >
+          {/* Attached */}
+          <FormControlLabel
+            value="proceedWithAttached"
+            control={<Radio size="small" />}
+            label="Process Attached Documents"
+          />
 
-              {/* Nested Options */}
-              <RadioGroup
-                value={nestedSelection}
-                onChange={(e) => setNestedSelection(e.target.value as any)}
-              >
-                {/* Attached Documents */}
-                <Box sx={{ display: "flex", flexDirection: "column" }}>
-                  <FormControlLabel
-                    value="proceedWithAttached"
-                    control={
-                      <Radio
-                        size="small"
-                        sx={{ p: 0.5, "& .MuiSvgIcon-root": { fontSize: 18 } }}
-                      />
-                    }
-                    label="Process Attached Documents"
-                  />
+          {nestedSelection === "proceedWithAttached" && (
+            <TextField
+              label="Add Number of Pages"
+              type="number"
+              value={numPages}
+              onChange={(e) => setNumPages(e.target.value)}
+              size="small"
+              sx={{ width: { xs: "100%", sm: "60%" } }}
+            />
+          )}
 
-                  {nestedSelection === "proceedWithAttached" && (
-                    <TextField
-                      label="Add Number of Pages"
-                      type="number"
-                      value={numPages}
-                      onChange={(e) => setNumPages(e.target.value)}
-                      size="small"
-                      sx={{ width: { xs: "100%", sm: "60%" }, marginTop: "6px", marginBottom: "6px" }}
-                    />
-                  )}
-                </Box>
+          {/* Original Mailed */}
+          <FormControlLabel
+            value="originalMailedNested"
+            control={<Radio size="small" />}
+            label={
+              country?.countryShortName === "Vietnam"
+                ? "Mail Original Documents to WCS office (after notarization & state certification)"
+                : "Mail Original Documents to WCS office"
+            }
+          />
 
-                {/* Original Mailed */}
-                <Box>
-                  <FormControlLabel
-                    value="originalMailedNested"
-                    control={
-                      <Radio
-                        size="small"
-                        sx={{ p: 0.5, "& .MuiSvgIcon-root": { fontSize: 18 } }}
-                      />
-                    }
-                    label={
-                      country?.countryShortName === "Vietnam"
-                        ? "Mail Original Documents to WCS office (after notarization & state certification)"
-                        : "Mail Original Documents to WCS office"
-                    }
-                  />
+          {nestedSelection === "originalMailedNested" && (
+            <Box sx={{ display: "flex", gap: 1, flexWrap: "wrap" }}>
+              <TextField
+                label="Tracking number to WCS"
+                value={trackingNumberNested}
+                onChange={(e) => setTrackingNumberNested(e.target.value)}
+                size="small"
+                sx={{ flex: 1 }}
+              />
 
-                  {nestedSelection === "originalMailedNested" && (
-                    <Box
-                      sx={{
-                        mt: 1,
-                        display: "flex",
-                        gap: 1,
-                        flexWrap: "wrap",
-                        width: "100%",
-                      }}
-                    >
-                      <TextField
-                        label="Tracking number to WCS"
-                        value={trackingNumberNested}
-                        onChange={(e) =>
-                          setTrackingNumberNested(e.target.value)
-                        }
-                        size="small"
-                        sx={{ flex: 1 }}
-                      />
-
-                      <Autocomplete
-                        options={couriers}
-                        value={courierNested}
-                        onChange={(_, value) => setCourierNested(value)}
-                        renderInput={(params) => (
-                          <TextField {...params} label="Courier" size="small" />
-                        )}
-                        sx={{ flex: 1, minWidth: 160 }}
-                      />
-                    </Box>
-                  )}
-                </Box>
-              </RadioGroup>
+              <Autocomplete
+                options={couriers}
+                value={courierNested}
+                onChange={(_, value) => setCourierNested(value)}
+                renderInput={(params) => (
+                  <TextField {...params} label="Courier" size="small" />
+                )}
+                sx={{ flex: 1, minWidth: 160 }}
+              />
             </Box>
           )}
-        />
-      </FormControl>
-    </Grid>
+        </RadioGroup>
+      </Box>
+    </FormControl>
   );
 }
