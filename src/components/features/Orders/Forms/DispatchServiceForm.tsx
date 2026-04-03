7df84803c2998b8d0a1feea7bf59c27ec0e8966b
Asish Kumar Behera <asish@redintegro.com>
2026-04-03 18:26:06 +0530

    Fixed the bugs releated to customer reference in U.S Authentication,Notary and dispatch service

--- src/components/features/Orders/Forms/DispatchServiceForm.tsx
+++ src/components/features/Orders/Forms/DispatchServiceForm.tsx
@@ -198,6 +198,7 @@ export default function DispatchServiceForm() {
       console.error(error);
       return false;
     } finally {
+      setCustomerReference("");
       setAdditionalComments(null);
       setIsSubmitting(false);
     }
--- src/components/features/Orders/Forms/NotaryServiceForm.tsx
+++ src/components/features/Orders/Forms/NotaryServiceForm.tsx
@@ -221,6 +221,7 @@ export default function NotaryServiceForm() {
       console.error(error);
       return false;
     } finally {
+      setCustomerReference("");
       setAdditionalComments(null);
       setIsSubmitting(false);
     }
